c299713ff4011d3079fcbb6558f996daea0305cb
imron481 <imron.gojali18@gmail.com>
2025-08-28 01:47:47 +0700

    init infrastructure middleware

---
+++ OrderService.Infrastructure/Middleware/TenantMiddleware.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace OrderService.Infrastructure.Middleware;
+
+public class TenantMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public TenantMiddleware(RequestDelegate next) => _next = next;
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var tenantIdStr = context.Request.Headers["X-Tenant-Id"].FirstOrDefault();
+        if (Guid.TryParse(tenantIdStr, out var tenantId))
+        {
+            context.Items["TenantId"] = tenantId;
+            context.Items["TenantSchema"] = tenantId == Guid.Parse("11111111-1111-1111-1111-111111111111") ? "tenant_a" : "tenant_b";
+        }
+        await _next(context);
+    }
+}

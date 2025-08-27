b8f3d932228d4d4d41247b6a22ef07c5258d7eb5
imron481 <imron.gojali18@gmail.com>
2025-08-28 01:46:27 +0700

    init domain entities

---
+++ OrderService.Domain/Entities/OrderItem.cs
@@ -0,0 +1,10 @@
+namespace OrderService.Domain.Entities;
+
+public class OrderItem
+{
+    public Guid Id { get; set; }
+    public Guid OrderId { get; set; }
+    public string ProductName { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+}

e1c041a3bd2758f4bc2d32f7cb372ce157b2e56b
imron481 <imron.gojali18@gmail.com>
2025-08-28 01:48:22 +0700

    init infrastructure services

---
+++ OrderService.Infrastructure/Services/TenantProvider.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Http;
+
+namespace OrderService.Infrastructure.Services;
+
+public class TenantProvider : ITenantProvider
+{
+    private readonly IHttpContextAccessor _ctx;
+
+    public TenantProvider(IHttpContextAccessor ctx)
+    {
+        _ctx = ctx;
+    }
+
+    public Guid GetTenantId()
+    {
+        var val = _ctx.HttpContext?.Items["TenantId"];
+        return val is Guid guid ? guid : Guid.Empty;
+    }
+
+    public string GetSchema()
+    {
+        var val = _ctx.HttpContext?.Items["TenantSchema"];
+        return val?.ToString() ?? "dbo";
+    }
+}

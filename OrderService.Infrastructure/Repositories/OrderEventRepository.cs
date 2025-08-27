7f0d6b6e9e59d4c37b1959bf895b03e28eb2fd17
imron481 <imron.gojali18@gmail.com>
2025-08-28 01:50:14 +0700

    init infrastructure repositories

---
+++ OrderService.Infrastructure/Repositories/OrderEventRepository.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using OrderService.Domain.Entities;
+using OrderService.Domain.Repositories;
+using OrderService.Infrastructure.Data;
+
+namespace OrderService.Infrastructure.Repositories;
+
+public class OrderEventRepository : IOrderEventRepository
+{
+    private readonly OrderDbContext _db;
+
+    public OrderEventRepository(OrderDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<List<OrderEvent>> GetEventsAsync(Guid orderId, Guid tenantId)
+    {
+        return await _db.OrderEvents
+            .Where(e => e.OrderId == orderId && e.TenantId == tenantId)
+            .ToListAsync();
+    }
+
+    public async Task AddEventAsync(OrderEvent orderEvent)
+    {
+        _db.OrderEvents.Add(orderEvent);
+        await _db.SaveChangesAsync();
+    }
+}

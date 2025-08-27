355ad203beb93a7c9b3c1169f4445105c2f077fb
imron481 <imron.gojali18@gmail.com>
2025-08-28 01:48:32 +0700

    init application service

---
+++ OrderService.Application/Services/OrderService.cs
@@ -0,0 +1,41 @@
+using OrderService.Domain.Entities;
+using OrderService.Domain.Repositories;
+
+namespace OrderService.Application.Services;
+
+public class OrderService
+{
+    private readonly IOrderRepository _orderRepo;
+    private readonly IOrderEventRepository _eventRepo;
+
+    public OrderService(IOrderRepository orderRepo, IOrderEventRepository eventRepo)
+    {
+        _orderRepo = orderRepo;
+        _eventRepo = eventRepo;
+    }
+
+    public async Task<List<Order>> GetOrders(Guid tenantId)
+    {
+        return await _orderRepo.GetAllAsync(tenantId);
+    }
+
+    public async Task<Order?> GetOrder(Guid id, Guid tenantId)
+    {
+        return await _orderRepo.GetByIdAsync(id, tenantId);
+    }
+
+    public async Task CreateOrder(Order order)
+    {
+        await _orderRepo.AddAsync(order);
+        var evt = new OrderEvent
+        {
+            Id = Guid.NewGuid(),
+            OrderId = order.Id,
+            TenantId = order.TenantId,
+            EventType = "Created",
+            Data = "Order created",
+            OccurredAt = DateTime.UtcNow
+        };
+        await _eventRepo.AddEventAsync(evt);
+    }
+}
